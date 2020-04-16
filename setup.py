ea86254c5a31e5d287529214eb6ca50b28c398ae
Pedro <pedromxavier@poli.ufrj.br>
2020-04-16 14:04:11 -0300

    <3

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ kwargs = {
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    "url" : 'https://pedromxavier.github.io/projects/satyrus',  # Optional
+    "url" : 'https://pedromxavier.github.io/projetos/satyrus',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
@@ -202,10 +202,10 @@ kwargs = {
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     "project_urls" : {  # Optional
-        'Bug Reports': 'https://pedromxavier.github.io/projects/satyrus',
-        'Funding': 'https://pedromxavier.github.io/projects/satyrus',
-        'Say Thanks!': 'https://pedromxavier.github.io/projects/satyrus',
-        'Source': 'https://pedromxavier.github.io/projects/satyrus',
+        'Bug Reports': 'https://pedromxavier.github.io/projetos/satyrus',
+        'Funding': 'https://pedromxavier.github.io/projetos/satyrus',
+        'Say Thanks!': 'https://pedromxavier.github.io/projetos/satyrus',
+        'Source': 'https://pedromxavier.github.io/projetos/satyrus',
     },
 }
 
